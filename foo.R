417b0106acfa80002824d71431eae6bfea95a7ca
ChrisP2000 <chrispang0202@gmail.com>
2023-10-23 11:17:54 +0800

    Update foo.R

--- foo.R
+++ foo.R
@@ -2,8 +2,9 @@
 r<-2
 
 #Function to compute the volumes of the spheres with radius r
+#volume of sphere 4/3 * pi *r^3
 volume <- function(r){
-  volumer<- 4/3*pi*r^2     #change from 3/4 to 4/3
+  volumer<- 4/3*pi*r^3     #change from 3/4 to 4/3
   return(volumer)
   
 }
@@ -12,15 +13,19 @@ volume(r)
   
 #functions to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r){
-  r <- 22
+ 
+  
   for (r2 in 1:3){
-
-    volume <- 4/3*pi*r^r2
-    print(paste(r2,'=',volume))
+    
+    r <- 22^r2
+    
+    volume(r)
+    print(volume(r))
     #volume(r)
-      
+    
 
   }
+  #return(volume(r))
 }
 
 #Run the volume_vector(r) and print the volumes of the spheres with
